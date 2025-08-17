af8b3d2c171775ce7799f6d8103cfe71d7616461
Abhinav Om <156777914+abhinavx0@users.noreply.github.com>
2025-08-18 05:02:21 +0530

    Update SELECT_questions.sql

--- leetcode_SQL 50/SELECT_questions.sql
+++ leetcode_SQL 50/SELECT_questions.sql
@@ -1,1 +1,7 @@
-
+-- Invalid Tweets.
+<img width="708" height="566" alt="Image" src="https://github.com/user-attachments/assets/46726568-09aa-4119-9acc-49e4be7b9c99" />
+```sql
+select tweet_id
+from tweets 
+where length(content) > 15;
+```
