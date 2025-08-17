e2e2df9934918d2aef8e898322957f371efe8152
Abhinav Om <156777914+abhinavx0@users.noreply.github.com>
2025-08-18 05:03:49 +0530

    Update SELECT_questions.sql

--- leetcode_SQL 50/SELECT_questions.sql
+++ leetcode_SQL 50/SELECT_questions.sql
@@ -1,5 +1,20 @@
 -- Invalid Tweets.
-<img width="708" height="566" alt="Image" src="https://github.com/user-attachments/assets/46726568-09aa-4119-9acc-49e4be7b9c99" />
+Table: Tweets
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| tweet_id       | int     |
+| content        | varchar |
++----------------+---------+
+tweet_id is the primary key (column with unique values) for this table.
+content consists of alphanumeric characters, '!', or ' ' and no other special characters.
+This table contains all the tweets in a social media app.
+ 
+
+Write a solution to find the IDs of the invalid tweets. The tweet is invalid if the number of characters used in the content of the tweet is strictly greater than 15.
+
+
 ```sql
 select tweet_id
 from tweets 
